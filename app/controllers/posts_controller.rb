365326328b4b777ab075afd7f721f994c17762a0
Elaine Anderson <eparie@bu.edu>
2018-08-02 21:03:17 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post!, only: [:show, :edit, :update, :create]
+  before_action :set_post!, only: [:show, :edit, :update]
 
   def create
       @post = Author.create!(author_params)
