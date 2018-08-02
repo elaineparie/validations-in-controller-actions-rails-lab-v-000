88ad4800e3f31ccfa0c543bd60aad8ad256a0bcd
Elaine Anderson <eparie@bu.edu>
2018-08-02 21:02:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+  def create
+      @post = Author.create!(author_params)
+    if @post.valid?
+      @post.save
+    redirect_to post_path(@post)
+  else
+    render :new
+  end
+  end
+
   def show
   end
 
